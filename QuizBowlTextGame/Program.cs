9f714e05c2892013b61a199987fb95b1064c4dc6
Cat Burner <DecCatBurner@gmail.com>
2024-08-03 11:27:30 -0500

    Prepare Syntax for Menu Revamp

--- QuizBowlTextGame/Program.cs
+++ QuizBowlTextGame/Program.cs
@@ -1,7 +1,10 @@
 // Game for Quiz Bowl Practice
 using System.Text.Json;
 using System.Collections;
+using System.Diagnostics;
 
+
+public enum LoopMode { Standard, Menu, Concept, Catagory, Question, Exit }
 
 // Game
 public class QuizBowlGame
@@ -71,84 +74,14 @@ public class QuizBowlGame
         }
     }
 
-    public static void Main(string[] args)
-    {
-        Console.Title = "____Quiz Bowler____";
-        Console.BackgroundColor = ConsoleColor.DarkBlue;
-        Console.ForegroundColor = ConsoleColor.Cyan;
-        // Interface vars
-        ConceptGroup[] concepts = new ConceptGroup[] {
-            AddConcept("CGMath.json"),
-            AddConcept("CGSports.json"),
-            AddConcept("CGMedia.json"),
-            AddConcept("CGScience.json"),
-            AddConcept("CGArt.json"),
-            AddConcept("CGHistory.json"),
-            AddConcept("CGPractice.json")
-        };
-        
-        List<IVec3> questionsAnswered = new(); 
-        List<int> finishedConcepts = new();
-        IVec3 questionC;
-        bool run = true;
-        var rand = new Random();
-        int correct = 0, wrong = 0, lengthRun = 1, streak = 0, highStreak = 0;
-        string Input = " ";
-        // Initial statements
-        Console.WriteLine("____Quiz Bowler____\n\u00A9 2024 DecCatBurner\nFor a list of commands type \"help\"");
-        SetMode: // Difficulty
-            bool practice = false;
-            Console.WriteLine("Do you want hints on? Y/N");
-            Input = Console.ReadLine();
-            if (Input.ToUpper() == "Y"){
-                practice = true;
-            } else if (Input.ToUpper() != "N"){
-                Console.WriteLine("Not an accepted input.\nEnter Y or N");
-                goto SetMode;
-            }
-            Console.WriteLine("\r");
-        // Questions
-        Run:
-            questionC.x = rand.Next(0, concepts.Length);
-            /*if (finishedConcepts.Contains(questionC.x)) {
-                goto Run;
-            }*/
-            ConceptGroup concept = concepts[questionC.x];
-            //int attemptCatagory = 0;
-        SetCatagory:
-            questionC.y = rand.Next(0, concept.catagories.Length);
-            /*for (int i = 0; i < concept.catagories.Length; i++){
-                if (i == questionC.y) {
-                    goto SetCatagory;
-                    attemptCatagory++;
-                }
-            }*/
-            QuestionCatagory catagory = concept.catagories[questionC.y];
-            //if (attemptCatagory == concept.catagories.Length) { goto Run; finishedConcepts.Add(questionC.x); }
-
-            lengthRun = catagory.length;
-        AskQuestion:
-            catagory.Print();
-            for (int i = 0; i < lengthRun; i++){
-                int attemptQuestion = 0;
-                questionC.z = rand.Next(0, catagory.questions.Length);
-                for (int j = 0; j < catagory.questions.Length; j++){
-                    if (j == questionC.z) {
-                        questionC.z = questionC.z++ % catagory.questions.Length;
-                        j = 0;
-                        attemptQuestion++;
-                        if (attemptQuestion > catagory.questions.Length) { j = catagory.questions.Length; }
-                    }
-                }
-                string Answer = catagory.questions[questionC.z].Print(practice);
-            	Input = Console.ReadLine();
-                // Check commands
-                switch (Input.ToUpper()){
+    public static void CheckCommand(string i) {
+        switch (i.ToUpper()){
                     case "SKIP": // Skip the current category
                         streak = 0;
-                        goto Run;
+                        break;
                     case "EXIT": // Exit the program
-                        goto Exit;
+                        run = LoopMode.Exit;
+                        break;
                     case "HELP": // List the commands
                         Help();
                         break;
@@ -156,7 +89,8 @@ public class QuizBowlGame
                         ListQuestions(concepts);
                         break;
                     case "DIFF": // Set the difficulty
-                        goto SetMode;
+                        run = LoopMode.Menu;
+                        break;
                     case "LENGTH": // Set the length
                         GetLength(out lengthRun);
                         break;
@@ -169,20 +103,24 @@ public class QuizBowlGame
                             concept = concepts[conceptID];
                         } else {
                             Console.WriteLine("Not a listed concept group.");
-                            goto AskQuestion;
+                            run = LoopMode.Question;
+                            lengthRun = 0;
                         }
                         if (catagoryID < concept.catagories.Length && catagoryID >= 0){
                             catagory = concept.catagories[catagoryID];
                         } else {
                             Console.WriteLine("Not a listed question catagory.");
                         }
-                        goto AskQuestion;
+                        run = LoopMode.Question;
+                        lengthRun = 0;
+                        break;
                     case "DATA": // Set the length
                         Console.WriteLine($"Correct: {correct}\nWrong: {wrong}\nCurrent Streak: {streak}\nHighest Streak: {highStreak}");
                         break;
                     default: // Handles answering the question
                         questionsAnswered.Add(new IVec3(questionC.x, questionC.y, questionC.z));
-                        if (Input.ToUpper() == Answer.ToUpper()){
+                        string answer = catagory.questions[questionC.z].Print(practice);
+                        if (i.ToUpper() == answer.ToUpper()){
                             correct++;
                             streak++;
                             if (streak > highStreak) { highStreak = streak; }
@@ -190,14 +128,101 @@ public class QuizBowlGame
                         }else{
                             wrong++;
                             streak = 0;
-                            Console.WriteLine($"Wrong. The correct answer is {Answer} \n\tCorrect:{correct}|Wrong:{wrong}");
+                            Console.WriteLine($"Wrong. The correct answer is {answer} \n\tCorrect:{correct}|Wrong:{wrong}");
                         }
                         break;
                 }
+    }
+
+    public static ConceptGroup[] concepts;
+    public static ConceptGroup concept;
+    public static QuestionCatagory catagory;
+    public static List<IVec3> questionsAnswered = new(); 
+    public static List<int> finishedConcepts = new();
+    public static IVec3 questionC;
+    public static bool practice = false;
+    public static LoopMode run = LoopMode.Standard;
+    public static Random rand = new Random();
+    public static int correct = 0, wrong = 0, lengthRun = 1, streak = 0, highStreak = 0;
+
+    public static void Main(string[] args)
+    {
+        Console.Title = "____Quiz Bowler____";
+        Console.BackgroundColor = ConsoleColor.DarkBlue;
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        // Interface vars
+        concepts = new ConceptGroup[] {
+            AddConcept("CGMath.json"),
+            AddConcept("CGSports.json"),
+            AddConcept("CGMedia.json"),
+            AddConcept("CGScience.json"),
+            AddConcept("CGArt.json"),
+            AddConcept("CGHistory.json"),
+            AddConcept("CGPractice.json")
+        };
+        
+        string input = " ";
+
+        // Initial statements
+        Console.WriteLine("____Quiz Bowler____\n\u00A9 2024 DecCatBurner\nFor a list of commands type \"help\"");
+        SetMode: // Difficulty
+            practice = false;
+            Console.WriteLine("Do you want hints on? Y/N");
+            input = Console.ReadLine();
+            if (input.ToUpper() == "Y"){
+                practice = true;
+            } else if (input.ToUpper() != "N"){
+                Console.WriteLine("Not an accepted input.\nEnter Y or N");
+                goto SetMode;
             }
-        if (run){
-    	    goto Run;
-        }
+            Console.WriteLine("\r");
+        // Questions
+        Run:
+            questionC.x = rand.Next(0, concepts.Length);
+            /*if (finishedConcepts.Contains(questionC.x)) {
+                goto Run;
+            }*/
+            concept = concepts[questionC.x];
+            //int attemptCatagory = 0;
+        SetCatagory:
+            questionC.y = rand.Next(0, concept.catagories.Length);
+            /*for (int i = 0; i < concept.catagories.Length; i++){
+                if (i == questionC.y) {
+                    goto SetCatagory;
+                    attemptCatagory++;
+                }
+            }*/
+        RunCatagory:
+            catagory = concept.catagories[questionC.y];
+            //if (attemptCatagory == concept.catagories.Length) { goto Run; finishedConcepts.Add(questionC.x); }
+
+            lengthRun = catagory.length;
+        AskQuestion:
+            if (run == LoopMode.Question) { run = LoopMode.Standard; }
+            catagory.Print();
+            for (int i = 0; i < lengthRun; i++){
+                int attemptQuestion = 0;
+                questionC.z = rand.Next(0, catagory.questions.Length);
+                for (int j = 0; j < catagory.questions.Length; j++){
+                    if (j == questionC.z) {
+                        questionC.z = questionC.z++ % catagory.questions.Length;
+                        j = 0;
+                        attemptQuestion++;
+                        if (attemptQuestion > catagory.questions.Length) { j = catagory.questions.Length; }
+                    }
+                }
+            	input = Console.ReadLine();
+                // Check commands
+                CheckCommand(input);
+            }
+            switch (run) {
+                case LoopMode.Standard:
+                    goto Run;
+                case LoopMode.Question:
+                    goto AskQuestion;
+                case LoopMode.Exit:
+                    goto Exit;
+            }
         Exit:
             Console.WriteLine("Exited Successfully.");
     }
--- QuizBowlTextGame/obj/Debug/net7.0/QuizBowlTextGame.AssemblyInfo.cs
+++ QuizBowlTextGame/obj/Debug/net7.0/QuizBowlTextGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuizBowlTextGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fc1f7beeee1f51df50297bc5948e644a9335fee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac3aeb73230cba6ea572ecd444d34ac274c02956")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuizBowlTextGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuizBowlTextGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
